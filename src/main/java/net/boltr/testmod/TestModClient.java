0639501ec044d14851e4d32122feacb0daaaa1f2
Boltr <boltr22@gmail.com>
2025-04-26 02:27:41 -0400

    T

---
+++ src/main/java/net/boltr/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.boltr.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
